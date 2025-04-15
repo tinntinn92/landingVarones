2a6c8784cae69fb24ccdd67d13a506ef5b1b3386
tinntinn92 <88331048+tinntinn92@users.noreply.github.com>
2025-04-15 16:07:39 -0300

    Proyecto base con Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'*/
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-2xl shadow-lg">
+        Tailwind está funcionando 🎉
+      </button>
+    </div>
+  );
+}
+
+export default App;
