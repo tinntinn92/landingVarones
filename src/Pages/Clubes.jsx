f74d6a9e6136387dd15d8e76c26ca34ad949ec9e
tinntinn92 <88331048+tinntinn92@users.noreply.github.com>
2025-04-15 21:26:01 -0300

    cambio icono principal

--- src/Pages/Clubes.jsx
+++ src/Pages/Clubes.jsx
@@ -4,16 +4,17 @@ import ClubCard from "../Components/ClubCard";
 function Clubes() {
   return (
     <div className="max-w-screen-xl mx-auto p-6">
-      
       <h1 className="text-2xl font-bold mb-4 text-center">Clubes de hockey sobre césped en Uruguay</h1>
-      <img
-          src={"/logos/cima.png"}
-          alt={`Logo`}
-          className="fixed top-4 right-12 w-48 object-contain z-50 "
-        />
       
+      {/* Imagen que solo será visible en pantallas grandes */}
+      <img
+        src={"/logos/cima.png"}
+        alt={`Logo`}
+        className="fixed top-4 right-12 w-48 object-contain z-50 hidden lg:block"  // Se oculta en pantallas pequeñas
+      />
+
       {/* Grid con 2 columnas para pantallas medianas y 3 para pantallas grandes */}
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {clubes.map((club, index) => (
           <ClubCard key={index} club={club} />
         ))}
@@ -23,6 +24,7 @@ function Clubes() {
 }
 
 export default Clubes;
+
 
 
 
